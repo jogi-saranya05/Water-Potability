5ee50f5222c27f18f4e1e12df52be414fde4c41b
Saranya Jogi <jogisara2@gmail.com>
2026-07-21 21:54:35 +0530

    Update app-2.py

--- app-2.py
+++ app-2.py
@@ -180,7 +180,7 @@ Write a simple 3-4 sentence explanation for a normal user.
         st.download_button(
             "📄 Download Report",
             report,
-            file_name="water_report.txt"
+            file_name="water_report.pdf"
         )
 
     with tab3:
